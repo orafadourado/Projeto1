d54c6e0d98b07605044ce5fb32c260f553daab64
orafadourado <65837417+orafadourado@users.noreply.github.com>
2021-11-03 03:58:11 -0300

    teste
    teste

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1635922518,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fish-sheet0.png",
+		"images/fish-sheet1.png",
+		"images/fish-sheet2.png",
+		"images/bg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
